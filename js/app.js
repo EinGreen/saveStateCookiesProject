f6f263f8697c173d7547ac6c90bcfd969aef72a4
Ein_Green <mbananola2002@gmail.com>
2021-03-27 00:03:39 -0700

    removed unnessisary code, added comments cause I forgot that was a requirement

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// this simple function makes a cookie of a chosen save file
 function selectSave(selectedSave) {
     Cookies.set(`selection`, selectedSave)
 }
--- js/app2.js
+++ js/app2.js
@@ -1,22 +1,25 @@
+// this block of code welcomes the user of the chosen Cookie
 var saveSelection = Cookies.get("selection");
 var selectionContainer = document.getElementById("saveDisplay");
 selectionContainer.innerHTML = `<p> Welcome! ${saveSelection} </p>`;
 
+// this block is used to create html tags to the selectionContainer
 var image = document.createElement("img");
 var message = document.createElement("p");
-var waitWhat = document.createElement("h4")
 selectionContainer.append(image);
 selectionContainer.append(message);
-selectionContainer.append(waitWhat);
 
+// this loop creates an Image to the respetive user
 if (saveSelection === "experiencedUser") {
     image.setAttribute("src", "../images/youAreWelcome.jpg");
 } else if (saveSelection === "worstUser") {
     image.setAttribute("src", "../images/rickRolled.gif");
     console.log("Hah ha ha, Hah Hah ha ha ha, ha ha ha, Hah");
+// I am aware of the cringe and I am !ashamed
 } else if (saveSelection === "newUser") {
     image.setAttribute("src", "../images/userCreation");
 } else {
+// this block of code only happens when a cookie is not selected
     image.setAttribute("src", "../images/waitWhat.jpg");
     console.log("Um, this guy didn't choose anything, what do I do?");
     console.log("I don't know, ask Jerry");
@@ -26,6 +29,7 @@ if (saveSelection === "experiencedUser") {
     message.innerText = "Dude, my guy, my bro, choose a freaking save. We can't work like this!"
 }
 
+// this function removes a cookie when you click the "Back" link
 function removeSaveCookie() {
     Cookies.remove(`selection`);
 }
